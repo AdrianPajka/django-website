7719d7cef8813d4368afcc64466949c680cee5ea
AdrianP <ad.pajka@gmail.com>
2020-04-27 14:26:39 +0200

    basic logic

--- posts/models.py
+++ posts/models.py
@@ -5,3 +5,9 @@ class Post(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add = True)
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:50]+ '...'
