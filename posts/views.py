3fb2b06634a5e2836a592d732c3c08a9593e6955
AdrianP <ad.pajka@gmail.com>
2020-04-28 07:53:29 +0200

    adding post detailed

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 def posts_list(request):
     posts = Post.objects.all().order_by('date')
     return render(request, 'posts/posts_list.html', {'posts': posts}  )
+
+def post_detail(request, slug):
+    # return HttpResponse(slug)
+    post = Post.objects.get(slug = slug)
+    return render(request, "posts/post_detail.html", {'post':post})
